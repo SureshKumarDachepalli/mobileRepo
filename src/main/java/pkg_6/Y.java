d7656cee5b63b8baf310be0089c6b3e9bd97cb4d
SureshKumarDachepalli <sureshkumar.d467@gmail.com>
2024-06-16 15:25:35 +0530

    mobele in commit messages-----

---
+++ src/main/java/pkg_6/Y.java
@@ -0,0 +1,16 @@
+package pkg_6;
+
+public class Y {
+
+	public static void main(String[] args) {
+		System.out.println("create y class in pkg-6");
+		
+		int a=100;
+		int b=200;
+		int c=a+b;
+		
+		System.out.println(c);
+
+	}
+
+}
---
+++ src/test/java/pkg_5/X.java
@@ -0,0 +1,14 @@
+package pkg_5;
+
+public class X {
+
+	public static void main(String[] args) {
+		int a=40;
+		int b=50;
+		int c=a+b;
+		
+		System.out.println("create X class in pkg-5:"+ c);
+
+	}
+
+}
