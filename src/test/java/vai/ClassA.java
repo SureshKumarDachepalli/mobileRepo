4296c52b494aecdf8f123c922218f51e321ddb6b
vaibhavby07 <vaibhavby07@gmail.com>
2024-06-16 18:41:36 +0530

    Creare new package vai with one class file check and review it

---
+++ src/test/java/vai/ClassA.java
@@ -0,0 +1,18 @@
+package vai;
+
+public class ClassA
+{
+   public static void method1()
+   {
+	   System.out.println("welcome");
+   }
+   public void method2()
+   {
+	   System.out.println("deat zindgi");
+   }
+   public static void main(String[] args) {
+	 ClassA obj=new ClassA();
+	 ClassA.method1();
+	 obj.method2();
+}
+}
